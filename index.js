f59eb1e3721dda9ebb22430e5b5626d6af0def41
Evan <evanrafuse.er@gmail.com>
2023-09-17 15:28:26 -0300

    searchbar added

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function main() {
-  console.log("TEST")
+  const searchForm = document.getElementById('searchForm');
+  const searchBox = document.getElementById('searchBox');
+  searchForm.addEventListener('submit', search);
+  urlBase = "https://www.google.com/search?q="
+  searchBox.focus()
 }
 
+function search(event) {
+  event.preventDefault()
+  console.log(searchBox.value)
+  url = urlBase + searchBox.value
+  window.open(url).focus()
+}
 main();
